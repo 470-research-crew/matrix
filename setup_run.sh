32e22dadf259232e50ec6cd41db6da72d481f56c
morri2wj <morri2wj@dukes.jmu.edu>
2024-04-26 14:08:00 -0400

    Update setup run to not install if unnecesssary

--- setup_run.sh
+++ setup_run.sh
@@ -1,24 +1,28 @@
 # set up environment
-git clone --recursive https://github.com/LLNL/RAJA.git
-mkdir RAJA-build
-mkdir RAJA-install
-mkdir RAJA-example
-echo 'Success in environment set up'
 
+if [[ ! -d RAJA-install ]]; then
+  git clone --recursive https://github.com/LLNL/RAJA.git
+  mkdir RAJA-build
+  mkdir RAJA-install
+  mkdir RAJA-example
+  echo 'Success in environment set up'
 
-# build RAJA 
-cd RAJA-build
-cmake -DCMAKE_INSTALL_PREFIX=../RAJA-install -DENABLE_OPENMP=On ../RAJA
-make -j && make install
-echo 'Success in build'
+  # build RAJA 
+  cd RAJA-build
+  cmake -DCMAKE_INSTALL_PREFIX=../RAJA-install -DENABLE_OPENMP=On ../RAJA
+  make -j && make install
+  echo 'Success in build'
 
-cd ../
+  cd ../
+fi
 
 # make and run program
 cp ./example/raja.cpp ./RAJA-example
 cp ./example/Makefile ./RAJA-example
 cd ./RAJA-example
 make
+
+cd ..
 
 echo 'Running program'
 ./run.sh
