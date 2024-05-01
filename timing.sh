9d438d4f839b17413175baed60bf5f55eca35b00
Mark Myers <myers2mt@dukes.jmu.edu>
2024-05-01 14:22:33 -0400

    Adding timing.sh code

--- timing.sh
+++ timing.sh
@@ -8,10 +8,10 @@
 sizes=(300 424 599 847 1197 1692 2392 3382 4782 6762 9562)
 threads=(1 2 4 8)
 serials=(1)
+cudas=(1)
 
 echo "Serial:"
 for t in "${serials[@]}"; do
-    export OMP_NUM_THREADS=$t
     for s in "${sizes[@]}"; do
         echo "Size: $s, Threads: $t"
         srun ./example/out/serial $s
@@ -33,10 +33,9 @@ printf "\n"
 printf "\n"
 echo "Pthread:"
 for t in "${threads[@]}"; do
-    export OMP_NUM_THREADS=$t
     for s in "${sizes[@]}"; do
         echo "Size: $s, Threads: $t"
-        srun ./example/out/pthread $s
+        srun ./example/out/pthread $s $t
     done
 done
 
@@ -54,8 +53,7 @@ done
 printf "\n"
 printf "\n"
 echo "Cuda:"
-for t in "${threads[@]}"; do
-    export OMP_NUM_THREADS=$t
+for t in "${cudas[@]}"; do
     for s in "${sizes[@]}"; do
         echo "Size: $s, Threads: $t"
         srun --gres=gpu ./example/out/cuda $s
